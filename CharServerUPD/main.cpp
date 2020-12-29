ed16016ec3719c572987aba1f54b1729ab66ef88
OnionSpirit <59094187+OnionSpirit@users.noreply.github.com>
2020-12-29 20:53:58 +0300

    Add files via upload

--- CharServerUPD/main.cpp
+++ CharServerUPD/main.cpp
@@ -4,36 +4,47 @@
 #include "sys/socket.h"
 #include <cstring>
 #include "netinet/in.h"
+#include <sstream>
+#include "thread"
 
-#define ERROR "ОШИБКА СЕРВЕРА: "
-#define NOTICE "СЕРВЕР: "
 #define BUFFER_SIZE 100
 #define CLOSE_SYMBOL '#'
+#define NOTICE "СЕРВЕР: "
+#define ERROR "ОШИБКА СЕРВЕРА: "
 #define EXTRA_PORT 8000
 
 
 bool Close_Connection(const char* s);
-void Cut_off(int* p, int serv);
+void Cut_off(int p, int serv, sockaddr_in client_address);
+void must_send();
+
+char buff[BUFFER_SIZE];
+bool check = false;
 
 int main(int argc, char* argv[]) {
-    int port_val = EXTRA_PORT;
-    int* port = &port_val;
-    try{
-        if(argc == 2) {
-            port = reinterpret_cast<int* >(argv[1]);
+    memset(buff, 0, BUFFER_SIZE);
+    int port = 0;
+    if(argc > 1){
+        std::stringstream convert(argv[1]);
+        if (!(convert >> port)) {
+            port = EXTRA_PORT;
+            std::cout << ERROR << "Пользовательские данные о порте не обнаружены.\n"
+                      << "\tУстановленно значение по умолчанию: " << port << std::endl;
         }
         else {
-            throw 1;
+            std::cout << NOTICE << "Пользовательское значение порта установленно. Значение порта: " << port <<  std::endl;
         }
     }
-    catch (int){
-        std::cout << ERROR << "Пользовательские данные о порте не обнаружены.\n" << "Установленно значение по умолчанию: " << EXTRA_PORT
-                  << std::endl;
+    else{
+        port = EXTRA_PORT;
+        std::cout << ERROR << "Пользовательские данные о порте не обнаружены.\n"
+                  << "\tУстановленно значение по умолчанию: " << port << std::endl;
     }
 
     int sock;
     struct sockaddr_in server_address, client_address;
     socklen_t size = sizeof(server_address);
+    int size_client = sizeof(client_address);
 
     sock = socket(AF_INET, SOCK_DGRAM, 0);
     if(sock < 0) {
@@ -41,7 +52,7 @@ int main(int argc, char* argv[]) {
         exit(0);
     }
 
-    server_address.sin_port = htons(*port);
+    server_address.sin_port = htons(port);
     server_address.sin_family = AF_INET;
     server_address.sin_addr.s_addr = htons(INADDR_ANY);
 
@@ -52,8 +63,6 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
-    int size_client = sizeof(client_address);
-
     std::cout << NOTICE <<"Сокет сервера успешно создан...\n";
 
     int RS;
@@ -61,34 +70,46 @@ int main(int argc, char* argv[]) {
     struct timeval timeout;
     FD_ZERO(&set);
     FD_SET(sock, &set);
-    timeout.tv_sec = static_cast<__time_t>(0.01);
+    timeout.tv_sec = 0;
     timeout.tv_usec = 0;
     char buffer[BUFFER_SIZE];
+    memset(buffer, 0, BUFFER_SIZE);
+    int process = 0;
+    std::thread s_thread(must_send);
+    sleep(1);
+    if(recvfrom(sock, buffer, BUFFER_SIZE, 0, reinterpret_cast<struct sockaddr*>(&client_address),
+                reinterpret_cast<socklen_t *>(&size_client)) == -1){
+        std::cout << ERROR << "Клиенту не удалось присоединиться" << std::endl;
+        Cut_off(port, sock, client_address);
+    }
+    memset(buffer, BUFFER_SIZE, 0);
+    std::cout << NOTICE << "Клиент подключён" << std::endl;
     std::cout << "Введите " << CLOSE_SYMBOL <<" для завершения сеанса соединения\n" << std::endl;
 
-    while(!Close_Connection(buffer)) {
 
-        std::cout << "Сервер: ";
-        std::cin.getline(buffer, BUFFER_SIZE);
-        sendto(sock, buffer, BUFFER_SIZE, 0, reinterpret_cast<struct sockaddr*>(&client_address),size_client);
+    while(!Close_Connection(buffer) && !Close_Connection(buff)){
+        if(check){
+            sendto(sock, buff, sizeof(buff), 0, reinterpret_cast<struct sockaddr*>(&client_address),size_client);
+            memset(buff, 0, BUFFER_SIZE);
+            check = false;
+        }
         RS = select(sock+1, &set, NULL, NULL, &timeout);
         FD_SET(sock, &set);
         if(RS <= 0){
             continue;
         }
-        else {
-            while(RS>0){
-                recvfrom(sock, buffer, BUFFER_SIZE, 0, reinterpret_cast<struct sockaddr*>(&client_address),
-                         reinterpret_cast<socklen_t *>(&size_client));
-                std::cout << "Клиент: ";
-                std::cout << buffer << std::endl;
-                RS = select(sock+1, &set, NULL, NULL, &timeout);
-                if(Close_Connection(buffer))
-                    break;
-            }
+        process = recvfrom(sock, buffer, BUFFER_SIZE, 0, reinterpret_cast<struct sockaddr*>(&client_address),
+                           reinterpret_cast<socklen_t *>(&size_client));
+        if(process == -1){
+            std::cout << ERROR << "Сообщение не может быть прочитано" << std::endl;
+            s_thread.detach();
+            break;
         }
+        std::cout << "Клиент: ";
+        std::cout << buffer<< std::endl;
     }
-    Cut_off(port, sock);
+    s_thread.detach();
+    Cut_off(port, sock, client_address);
 }
 
 bool Close_Connection(const char* s) {
@@ -99,10 +120,18 @@ bool Close_Connection(const char* s) {
     }
     return false;
 }
-void Cut_off(int* p, int serv){
+void Cut_off(int p, int serv, sockaddr_in client_address){
+    int size_client = sizeof(client_address);
+    sendto(serv, "#", sizeof("#"), 0, reinterpret_cast<struct sockaddr*>(&client_address),size_client);
     shutdown(serv, 2);
-    std::cout << std::endl << NOTICE <<"Соединение разорвано\n";
+    std::cout << std::endl << NOTICE <<"Сеанс соединения окончен\n";
     close(serv);
-    std::cout << "Порт " << *p << " снова доступен\n";
+    std::cout << "Порт " << p << " снова доступен\n";
     exit(2);
+}
+void must_send(){
+    while(!Close_Connection(buff)){
+        std::cin.getline(buff, sizeof(buff), '\n');
+        check = true;
+    }
 }
--- ChatClientUPD/main.cpp
+++ ChatClientUPD/main.cpp
@@ -5,6 +5,8 @@
 #include "sys/socket.h"
 #include <cstring>
 #include "netinet/in.h"
+#include <sstream>
+#include "thread"
 
 #define LOCAL_HOST "127.0.0.1"
 #define BUFFER_SIZE 100
@@ -14,24 +16,31 @@
 #define EXTRA_PORT 8000
 
 bool Close_Connection(const char* s);
-void Cut_off(int* p, int cli);
+void Cut_off(int cli, sockaddr_in server_address);
+void must_send();
+
+char buff[BUFFER_SIZE];
+bool check = false;
 
 int main(int argc, char* argv[]) {
-    const char *IP = LOCAL_HOST;
-    int port_1 = EXTRA_PORT;
-    int *port = &port_1;
-    try {
-        if (argc > 2) {
-           *port = (atoi(argv[argc]));
-            IP = reinterpret_cast<const char * >(argv[argc]);
-        } else {
-            throw 1;
+    memset(buff, 0, BUFFER_SIZE);
+    const char* IP;
+    int port;
+    if(argc > 1){
+        std::stringstream convert(argv[1]);
+        if (!(convert >> port)) {
+            port = EXTRA_PORT;
+            std::cout << ERROR << "Пользовательские данные о порте не обнаружены.\n"
+                      << "\tУстановленно значение по умолчанию: " << port << std::endl;
+        }
+        else {
+            std::cout << NOTICE << "Пользовательское значение порта установленно. Значение порта: " << port <<  std::endl;
         }
     }
-    catch (int) {
+    else{
+        port = EXTRA_PORT;
         std::cout << ERROR << "Пользовательские данные о порте не обнаружены.\n"
-                  << "Установленно значение по умолчанию: " << EXTRA_PORT
-                  << std::endl;
+                  << "\tУстановленно значение по умолчанию: " << port << std::endl;
     }
     int sock;
 
@@ -43,47 +52,78 @@ int main(int argc, char* argv[]) {
     }
     int size = sizeof(server_address);
     int size_client = sizeof(client_address);
-    server_address.sin_port = htons(*port);
+    server_address.sin_port = htons(port);
     server_address.sin_family = AF_INET;
-    inet_pton(AF_INET, IP, &server_address.sin_addr);
+    if(!(IP = argv[2])){
+        IP = LOCAL_HOST;
+        std::cout << ERROR << "Пользовательские данные об IP не обнаружены.\n"
+                  << "\tУстановленно значение по умолчанию: " << IP << std::endl;
+        inet_pton(AF_INET, IP, &server_address.sin_addr);
+    }
+    else {
+        if (!inet_pton(AF_INET, IP, &server_address.sin_addr)) {
+            IP = LOCAL_HOST;
+            std::cout << ERROR << "Данные об IP введены некорректно.\n"
+                                  "\tУстановленно значение по умолчанию: " << IP << std::endl;
+            inet_pton(AF_INET, IP, &server_address.sin_addr);
+        } else {
+            std::cout << NOTICE << "Пользовательское значение IP установленно. Значение IP: " << IP << std::endl;
+        }
+    }
     client_address.sin_family = AF_INET;
     client_address.sin_port = htons(0);
     client_address.sin_addr.s_addr = htons(INADDR_ANY);
 
-    bind(sock, reinterpret_cast<struct sockaddr*>(&client_address), size_client);
+    if(bind(sock, reinterpret_cast<struct sockaddr*>(&client_address), size_client) < 0){
+        std::cout << ERROR
+                  << "Сокет который вы пытетесь открыть уже занят.\n";
+        return -1;
+    }
 
     int RS;
     fd_set set;
     struct timeval timeout;
     FD_ZERO(&set);
     FD_SET(sock, &set);
-    timeout.tv_sec = static_cast<__time_t>(0.01);
+    timeout.tv_sec = 0;
     timeout.tv_usec = 0;
-        char buffer[BUFFER_SIZE];
-        std::cout << "Введите " << CLOSE_SYMBOL << " для завершения сеанса соединения\n" << std::endl;
+    char buffer[BUFFER_SIZE];
+    memset(buffer, 0, BUFFER_SIZE);
+    int process = 0;
+    std::thread s_thread(must_send);
+    sleep(1);
+    if(sendto(sock, "1", sizeof("1"), 0, reinterpret_cast<struct sockaddr*>(&server_address), size) == -1){
+        std::cout << ERROR << "Не удалось отправить запрос серверу" << std::endl;
+        Cut_off(sock, server_address);
+    }
+    std::cout << NOTICE << "Подключён к серверу" << std::endl;
+    std::cout << "Введите " << CLOSE_SYMBOL << " для завершения сеанса соединения\n" << std::endl;
 
-        while (!Close_Connection(buffer)) {
-            std::cout << "Клиент: ";
-            std::cin.getline(buffer, BUFFER_SIZE);
-            sendto(sock, buffer, BUFFER_SIZE, 0, reinterpret_cast<struct sockaddr*>(&server_address), size);
+
+
+    while(!Close_Connection(buffer) && !Close_Connection(buff)){
+        if(check){
+            sendto(sock, buff, sizeof(buff), 0, reinterpret_cast<struct sockaddr*>(&server_address), size);
+            memset(buff, 0, BUFFER_SIZE);
+            check = false;
+        }
         RS = select(sock+1, &set, NULL, NULL, &timeout);
         FD_SET(sock, &set);
         if(RS <= 0){
             continue;
         }
-        else {
-            while(RS>0){
-            recvfrom(sock, buffer, BUFFER_SIZE, 0, reinterpret_cast<struct sockaddr*>(&server_address),
-                     reinterpret_cast<socklen_t *>(&size));
-            std::cout << "Сервер: ";
-            std::cout << buffer << std::endl;
-                RS = select(sock+1, &set, NULL, NULL, &timeout);
-                if(Close_Connection(buffer))
-                    break;
-            }
+        process = recvfrom(sock, buffer, BUFFER_SIZE, 0, reinterpret_cast<struct sockaddr*>(&server_address),
+                           reinterpret_cast<socklen_t *>(&size));
+        if(process == -1){
+            std::cout << ERROR << "Сообщение не может быть прочитано" << std::endl;
+            s_thread.detach();
+            break;
         }
-        }
-        Cut_off(port, sock);
+        std::cout << "Сервер: ";
+        std::cout << buffer<< std::endl;
+    }
+    s_thread.detach();
+    Cut_off(sock, server_address);
 }
 
 
@@ -95,10 +135,19 @@ bool Close_Connection(const char* s) {
     }
     return false;
 }
-void Cut_off(int* p, int cli){
+void Cut_off(int cli, sockaddr_in server_address){
+    int size = sizeof(server_address);
+    sendto(cli, "#", sizeof("#"), 0, reinterpret_cast<struct sockaddr*>(&server_address), size);
     std::cout << std::endl<< NOTICE << "Завершение сеанса соединения. Закрываю сокет\n";
     shutdown(cli, 2);
     close(cli);
     std::cout << NOTICE << "Сокет закрыт.\n";
     exit(2);
 }
+void must_send(){
+    while(!Close_Connection(buff)){
+        std::cin.getline(buff, sizeof(buff), '\n');
+        check = true;
+    }
+}
+//бинды проверь
