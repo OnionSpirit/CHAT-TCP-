280758f2df9d7ffc74102d11a218ca3067d9a58e
OnionSpirit <59094187+OnionSpirit@users.noreply.github.com>
2021-01-07 15:09:41 +0300

    Update main.cpp

--- ChatClientUPD/main.cpp
+++ ChatClientUPD/main.cpp
@@ -86,7 +86,7 @@ int main(int argc, char* argv[]) {
     FD_ZERO(&set);
     FD_SET(sock, &set);
     timeout.tv_sec = 0;
-    timeout.tv_usec = 0;
+    timeout.tv_usec = 1000;
     char buffer[BUFFER_SIZE];
     memset(buffer, 0, BUFFER_SIZE);
     int process = 0;
